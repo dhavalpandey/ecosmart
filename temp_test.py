6a63f88398a9b4906bc5c135b087392957a8213a
Dhaval Pandey <dhavalpandey@Dhavals-MacBook-Air.local>
2025-03-19 13:40:12 +0000

    temp3

--- temp_test.py
+++ temp_test.py
@@ -1,10 +1,9 @@
 import RPi.GPIO as GPIO
-
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-p = GPIO.PWM(14, 50)
+p = GPIO.PWM(11, 50)
 p.start(0)
 
 sleep(0.1)
@@ -17,6 +16,8 @@ try:
         sleep(1)
 except KeyboardInterrupt:
     print('s')
+except Exception as e:
+    print(f'An error occurred: {e}')
 finally:
     p.stop()
     GPIO.cleanup()
